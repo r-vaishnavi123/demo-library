0523947045320e04cf996002aa9ce633b78ade17
manyajainn <manya.jain@mphasis.com>
2026-04-08 15:43:59 +0530

    book controller added

---
+++ library-service/src/main/java/com/library/service/library_service/controller/BookController.java
@@ -0,0 +1,32 @@
+package com.library.service.library_service.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+ 
+import org.springframework.web.bind.annotation.*;
+ 
+@RestController
+@RequestMapping("/books")
+public class BookController {
+ 
+    private List<String> books = new ArrayList<>();
+ 
+    @PostMapping
+    public String addBook(@RequestBody String bookName) {
+        books.add(bookName);
+        return "Book added: " + bookName;
+    }
+ 
+    @GetMapping
+    public List<String> getBooks() {
+        return books;
+    }
+ 
+    @GetMapping("/{id}")
+    public String getBookById(@PathVariable int id) {
+        if (id < books.size()) {
+            return books.get(id);
+        }
+        return "Book not found";
+    }
+}
